425e1cc7e346f454a3e3641c684bc7d82e31a26f
Kaustubh789 <41884472+Kaustubh789@users.noreply.github.com>
2018-07-30 10:18:47 +0530

    Add files via upload

---
+++ myProj.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void checkOddEven(int n);
+
+int main()
+{
+    int n;
+    cout<<"Enter any number : ";
+    cin>>n;
+    checkOddEven(n);
+}
+
+void checkOddEven(int n)
+{
+    if(n%2){
+        cout<<n<<" is Odd";
+    }
+    else
+        cout<<n<<" is Even";
+}
